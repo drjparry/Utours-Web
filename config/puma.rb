23cb845f29df1016a9b9e7f7bafcd0129db3a491
David Parry <david.parry@trustedfamily.net>
2018-06-13 23:08:20 +0200

    puma port fix

--- config/puma.rb
+++ config/puma.rb
@@ -11,7 +11,7 @@ threads threads_count, threads_count
 preload_app!
 
 rackup      DefaultRackup
-port        ENV['PORT']     || 3000
+# port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
@@ -25,7 +25,7 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-# port        ENV.fetch("PORT") { 3000 }
+port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
